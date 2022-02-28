b880f1c69b517cdaac9999878c5f72db4b645e82
sj-fleming <sarah.fleming22@bishopireton.org>
2022-02-28 13:19:50 -0500

    added process method

--- MessagePriorityQueue/src/MessagePriorityQueue.java
+++ MessagePriorityQueue/src/MessagePriorityQueue.java
@@ -14,6 +14,30 @@ public class MessagePriorityQueue {
 	static final int NUM_MESSAGES = 10;
 	static final int NUM_PRIORITIES = 5;
 	static final int NUM_TESTS = 100;
+	
+	public static void process(int i) {
+		Message m;
+		if(queue.get(0) != null) {
+			m = queue.get(0).remove();
+			System.out.println("Message processed. Priority: " + m.getPriority() + "Time: " + i + "Arrival: " + m.getArrivalTime());
+		}
+		else if(queue.get(1) != null) {
+			m = queue.get(1).remove();
+			System.out.println("Message processed. Priority: " + m.getPriority() + "Time: " + i + "Arrival: " + m.getArrivalTime());
+		}
+		else if(queue.get(2) != null) {
+			m = queue.get(2).remove();
+			System.out.println("Message processed. Priority: " + m.getPriority() + "Time: " + i + "Arrival: " + m.getArrivalTime());
+		}
+		else if(queue.get(3) != null) {
+			m = queue.get(3).remove();
+			System.out.println("Message processed. Priority: " + m.getPriority() + "Time: " + i + "Arrival: " + m.getArrivalTime());
+		}
+		else if(queue.get(4) != null) {
+			m = queue.get(4).remove();
+			System.out.println("Message processed. Priority: " + m.getPriority() + "Time: " + i + "Arrival: " + m.getArrivalTime());
+		}				
+	}
 
 	public static void main(String[] args) {
 		queue = new ArrayList<Queue<Message>>(5);
@@ -34,7 +58,7 @@ public class MessagePriorityQueue {
 			queue.get(priority).add(new Message(priority, i));
 			//remove and process
 			if (i+1 % 4 == 0) {
-				
+				process(i);
 			}
 		}
 		
